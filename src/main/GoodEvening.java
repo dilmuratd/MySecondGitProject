b63a3c14ec6e910b7dfdb4055ce5dd07f7b8f658
dilmuratd <dilmuratd@gmail.com>
2018-03-10 12:21:32 -0500

    initialize commit

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -5,6 +5,8 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Good Evening Class!");
+	
+	System.out.println("My name is Maria");
 	}
 
 }
