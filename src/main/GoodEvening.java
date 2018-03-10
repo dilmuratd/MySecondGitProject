06823d4bda4b03486953bf8dab4fec1c2a6cb83d
afsheensiddiki <afsheensiddiki94@gmail.com>
2018-03-10 14:45:54 -0500

    changed

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -5,8 +5,9 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Good Evening Class!");
-	
 	System.out.println("My name is Dilmurat");
+	System.out.println("What's good?");
+	System.out.println("kuch nahin");
+	System.out.println("main bhi theek hun ");
 	}
-
 }
